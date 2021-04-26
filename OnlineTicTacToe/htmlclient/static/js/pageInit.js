f657485b71f0c8b8887339b1b567c13f7d746ca5
ultraflame4 <ultraflame4@gmail.com>
2021-04-26 20:48:17 +0800

    - backup. yay more errors

--- OnlineTicTacToe/htmlclient/static/js/pageInit.js
+++ OnlineTicTacToe/htmlclient/static/js/pageInit.js
@@ -6,22 +6,23 @@ function initOnLoad() {
     disablePlayButtons()
     console.log("Setting promise resolve callback")
     languagePluginLoader.then(() => {
-            // Pyodide is now ready to use...
-            let package_path = new URL("../../../OnlineTicTacToe/dist/OnlineTicTacToe-2.0.0-py2.py3-none-any.whl", window.location).href
-            console.log(package_path)
-            pyodide.runPythonAsync(`
-            import micropip
-            print("${package_path}","f")
-            await micropip.install('${package_path}')
-            `)
+        // Pyodide is now ready to use...
+        let package_path = new URL("../../../OnlineTicTacToe/dist/OnlineTicTacToe-2.0.0-py2.py3-none-any.whl", window.location).href
+        console.log(package_path)
+        pyodide.runPythonAsync(`
+        import micropip
+        await micropip.install('${package_path}')
+        from OnlineTicTacToe import core as gameCore
+        gameCore.testimport()
+        `,pyInited)
 
-        pyInited()
-        gameUiInit()
+
 
         enablePlayButtons()
     });
 
 }
+
 
 
 function pyInited(){
