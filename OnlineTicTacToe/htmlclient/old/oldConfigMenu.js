da7826eb5fa6beb969c5f8e52483974949d4180e
ultraflame4 <ultraflame4@gmail.com>
2021-06-07 13:36:35 +0800

    - added old html

---
+++ OnlineTicTacToe/htmlclient/old/oldConfigMenu.js
@@ -0,0 +1,48 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("menuMainButton").click()
+    setAllSquareDisabled()
+
+})
+
+
+function resetMenuButtons() {
+    let menuButtons = document.getElementsByClassName("menuButton")
+    for (let i = 0; i < menuButtons.length; i++) {
+        t = menuButtons.item(i)
+        t.dataset.state = "inactive"
+    }
+}
+
+function resetMenuPages() {
+    let menuPages = document.getElementsByClassName("menuPage")
+    for (let i = 0; i < menuPages.length; i++) {
+        t = menuPages.item(i)
+        t.setAttribute("hidden", "hidden")
+
+    }
+}
+
+function openMenu(menuId, tagId) {
+    resetMenuButtons()
+    resetMenuPages();
+    document.getElementById(tagId).dataset.state = "active"
+    if (menuId != "none")
+        document.getElementById(menuId).removeAttribute("hidden")
+}
+
+
+function openMainMenu() {
+    openMenu("MainMenuPage", "menuMainButton")
+}
+
+function openJoinMenu() {
+    openMenu("joinMenuPage", "menuJoinButton")
+}
+
+function openHostMenu() {
+    openMenu("HostMenuPage", "menuHostButton")
+}
+
+function openGameInfoMenu() {
+    openMenu("GameInfoMenuPage", "menuGameButton")
+}
